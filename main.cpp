437d7777ce70bd7046e5a465d79b1194276b93d1
alx <alx.yulax@gmail.com>
2022-01-04 20:00:46 +0700

    вместо Track **available_records; можно использовать std::vector<Track> available_records;

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <cassert>
 #include <iomanip>
+#include <vector>
 
 class Track {
     std::string title;
@@ -39,27 +40,31 @@ class Player {
     int track_number;
     int track_start;
     std::time_t track_time_left;
-    Track **available_records;
+    //Track **available_records;
+    std::vector<Track> available_records_v;
+
+
 public:
     Player() : status(STOPPED), track_number(-1) {
-        available_records = new Track *[5];
+        //available_records = new Track *[5];
 
         std::tm released{};
         released.tm_year = 119;
         released.tm_mon = 5;
         released.tm_mday = 1;
 
-        available_records[0] = new Track("Just Like a Pill", released, 244);
-        available_records[1] = new Track("Who Knew", released, 212);
-        available_records[2] = new Track("Time After Time", released, 206);
-        available_records[3] = new Track("Just Give Me a Reason", released, 278);
-        available_records[4] = new Track("Walk Me Home", released, 177);
+
+        available_records_v.push_back(Track("Just Like a Pill", released, 244));
+        available_records_v.push_back(Track("Who Knew", released, 212));
+        available_records_v.push_back(Track("Time After Time", released, 206));
+        available_records_v.push_back(Track("Just Give Me a Reason", released, 278));
+        available_records_v.push_back(Track("Walk Me Home", released, 177));
 
     }
 
     ~Player() {
-        for (int i = 0; i < 5; ++i) {
-            delete available_records[i];
+        for (int i = 0; i < available_records_v.size(); ++i) {
+            //delete available_records[i];
         }
     }
 
@@ -76,8 +81,8 @@ public:
         update_status_PLAYING();
         if (status == STOPPED) {
             if (in_track_number == -1) {
-                for (int i = 0; i < 5; ++i) {
-                    std::cout << i << " " << available_records[i]->getTitle() << std::endl;
+                for (int i = 0; i < available_records_v.size(); ++i) {
+                    std::cout << i << " " << available_records_v[i].getTitle() << std::endl;
                 }
                 std::cout << "Input number of track (0-4): ";
                 std::cin >> in_track_number;
@@ -87,20 +92,20 @@ public:
             } else {
                 track_number = in_track_number;
                 track_start = std::time(nullptr);
-                track_time_left = available_records[in_track_number]->getDuration_sec();
+                track_time_left = available_records_v[in_track_number].getDuration_sec();
                 status = PLAYING;
 
                 std::cout << "PLAYING: " << std::endl;
-                available_records[in_track_number]->show();
+                available_records_v[in_track_number].show();
             }
         } else if (status == PLAYING) {
             std::cout << "NOW playing: " << std::endl;
-            available_records[track_number]->show();
+            available_records_v[track_number].show();
         } else if (status == PAUSED) {
             track_start = std::time(nullptr);
             status = PLAYING;
             std::cout << "Continue playing: " << std::endl;
-            available_records[track_number]->show();
+            available_records_v[track_number].show();
         }
     }
 
@@ -109,7 +114,7 @@ public:
         if (status == PLAYING) {
             status = PAUSED;
             std::cout << "PAUSED: " << std::endl;
-            available_records[track_number]->show();
+            available_records_v[track_number].show();
         }
     }
 
@@ -125,7 +130,7 @@ public:
         if (status != STOPPED) {
             if (update_status_PLAY) {
                 std::cout << "STOPPED: " << std::endl;
-                available_records[track_number]->show();
+                available_records_v[track_number].show();
             }
             track_start = 0;
             track_time_left = 0;
